227fadd6bb15be05609aa02aea3683a378f94b6d
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-11-21 14:33:41 +0200

    Add social media wesites

--- app.js
+++ app.js
@@ -6,3 +6,37 @@ $(document).ready(function(){
         dotsClass: 'dots'
     })
 });
+
+// Form Validation
+const form = document.querySelector('.form-input');
+const email = document.getElementById('email');
+
+// Error Mesaage for Invalid email
+const setError = (message) => {
+  const errorDisplay = document.querySelector('.error');
+  errorDisplay.innerText = message;
+};
+
+// Success Mesaage for Valid email
+const setSuccess = (message) => {
+  const errorDisplay = document.querySelector('.error');
+  errorDisplay.innerText = message;
+};
+const validateInputs = (e) => {
+    const emailValue = email.value.trim();
+  
+    if (emailValue === '') {
+      setError('Email is required');
+      e.preventDefault();
+    } else if (emailValue.toLowerCase() !== emailValue) {
+      setError('email should be in lowercase. Please resubmit again');
+      e.preventDefault();
+    } else {
+      setSuccess('Data sent Successfully');
+    }
+  };
+  
+  form.addEventListener('submit', (e) => {
+    validateInputs(e);
+  });
+  
