331c1d48162a091a61fdf0e3ed0c0c74dd6565b4
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-11-22 01:40:14 +0200

    Use media query to be responsive

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ $(document).ready(function(){
         dots: true,
         appendDots: '.slider-dot',
         dotsClass: 'dots'
-    })
+    });
+    let humberger = document.querySelector('.hamberger-button');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+    let click = document.querySelector('.click');
+    let click1 = document.querySelector('.click-1');
+    let click2 = document.querySelector('.click-2');
+    let click3 = document.querySelector('.click-3');
+
+    humberger.addEventListener('click', function(){
+      mobileNav.classList.add('open');
+    });
+
+    times.addEventListener('click', function(){
+      mobileNav.classList.remove('open');
+    });
+
+    click.addEventListener('click', function(){
+      mobileNav.classList.remove('open');
+    });
+    click1.addEventListener('click', function(){
+      mobileNav.classList.remove('open');
+    });
+    click2.addEventListener('click', function(){
+      mobileNav.classList.remove('open');
+    });
+    click3.addEventListener('click', function(){
+      mobileNav.classList.remove('open');
+    });
+
 });
 
 // Form Validation
@@ -40,3 +69,4 @@ const validateInputs = (e) => {
     validateInputs(e);
   });
   
+
